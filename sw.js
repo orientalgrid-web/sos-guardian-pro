b107726348bbcc64fba853bce2cac70cadb6c05c
orientalgrid-web <orientalgrid@gmail.com>
2026-02-05 05:43:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-// SafeRoute Service Worker
+// Simple Service Worker
 const CACHE_NAME = 'saferoute-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 // Install
@@ -21,17 +21,4 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
-    ).then(() => self.clients.claim())
-  );
-});
-
-// Fetch
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+      Promise
